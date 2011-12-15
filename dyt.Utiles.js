5f31d06788e34b94dded270864dc7ac527e78c83
apl330 <apl330@gmail.com>
2011-12-15 09:07:27 +0800

    add the initArea method

--- dyt.Utiles.js
+++ dyt.Utiles.js
@@ -1,4 +1,5 @@
-;(function($){
+;
+(function($) {
 	// 邮箱
 	reg_email = /\w{3,}@\w+(\.\w+)+$/;
 	// 中文
@@ -7,9 +8,9 @@
 	reg_DByte = /[^\x00-\xff]/;
 	// 正实数
 	reg_Floating_Positive = /^(0|[1-9]\d*)(\.\d*)?$/;
-	//移动电话
+	// 移动电话
 	reg_mobil_phone = /^(?:13\d|15\d)\d{5}(\d{3}|\*{3})$/;
-	//家用电话
+	// 家用电话
 	reg_phone = /^((0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;
 
 	// 公共方法工具包
@@ -35,11 +36,11 @@
 					isFPositive :  function(obj){
 						return $.dyt.validator.common(reg_Floating_Positive, obj);
 					},
-					//移动电话
+					// 移动电话
 					isMobilePh  : function(obj){
 						return $.dyt.validator.common(reg_mobil_phone, obj);
 					},
-					//家用电话
+					// 家用电话
 					isPhone     : function(obj){
 						return $.dyt.validator.common(reg_phone, obj);
 					},
@@ -59,7 +60,7 @@
 			},
 			// 表单验证
 			form_validate : {
-				　// 表单值不能为空
+				// 表单值不能为空
 				 required : function(fieldId, promptDivId, promptMsg){
 					 if($.trim($(fieldId).val()) == ""){
 						 return false;
@@ -80,12 +81,17 @@
 	/** ---------------这里的.warn需要重构----------------* */
 	
 	/**
-	 *　@param　boolean required　是否必须
-	 *  @param　string req_prompt　表单为空时的提示语
-	 *  @param　string url ajax查询的地址
-	 *  @param string key json数据的key,　如{key : value}
-	 *  @param string exist_prompt 已存在时的提示语
-	 *  @author zjzhai
+	 * @param boolean
+	 *            required 是否必须
+	 * @param string
+	 *            req_prompt 表单为空时的提示语
+	 * @param string
+	 *            url ajax查询的地址
+	 * @param string
+	 *            key json数据的key, 如{key : value}
+	 * @param string
+	 *            exist_prompt 已存在时的提示语
+	 * @author zjzhai
 	 */
 	$.fn.dyt_required_exist = function(options ){
 		var settings = $.extend({
@@ -160,7 +166,7 @@
 		return this;
 	};
 	
-	//电话
+	// 电话
 	$.fn.dyt_isPhone = function(prompt, options){
 		var settings = $.extend({
 			required : false,
@@ -183,9 +189,93 @@
 		
 	};
 	
-	
+	// 地域级联选择
+	/**
+	 * 接收的json数据格式：{"areas":[{"code":"341500","fullName":"安徽省六安市","id":42,"name":"六安市","new":false,"version":1},
+	 * 																{"code":"340800","fullName":"安徽省安庆市","id":32,"name":"安庆市","new":false,"version":1}
+	 * 																]}
+	 * @param postDataName 传往服务器的参数名，格式：{postDataName:value}
+	 * @param action 返回地域的json数据
+	 * @param select 空白时显示的字符，默认值：请选择
+	 * @author zjzhai
+	 */
+	$.fn.initArea = function(action, postDataName, options){
+		var settings = $.extend({
+			select   : '请选择'
+		}, options || {});
+		
+		var getAreaUrl = action;
+		var self = $(this);
+		// 初始化'省'
+		$.post(
+			getAreaUrl,
+			function(data){
+				$.each(data.areas,function(i, item){
+					$("<option></option>").attr("value",item.id).html(item.name).appendTo($(self));
+				});
+			}
+		);
+		$(self).after($('<input type="hidden" id="dy_hidden_area_input" />').attr('name', postDataName));
+		
+		// 省级
+		$(self).bind("change",function(){
+			$("#city").remove();
+			$("#district").remove();
+			setAreaValue($(self).val());
+			$.post(
+					getAreaUrl,
+					{'areaId':$(self).val()},
+					function(data){
+						if(data.areas.length > 0){
+							// 市级
+							createCity(data.areas);
+						}
+					}
+				);
+		});
+		
+		//市级
+		function createCity(areas){
+			$(self).after($("<select id='city'></select>").append($('<option></option>').html(settings.select)));
+			$.each(areas,function(i, item){
+				$('#city').append($("<option></option>").attr("value",item.id).html(item.name));
+			});
+			$('#city').bind("change",function(){
+				$('#district').remove();
+				setAreaValue($('#city').val());
+				$.post(
+						getAreaUrl,
+						{'areaId':$('#city').val()},
+						function(data){
+							createDistrict(data.areas);
+						});
+			});
+		}
+		
+		//县级
+		function createDistrict(areas){
+			if(areas[0]!=null){
+				$("#city").after($("<select id='district'></select>").append($('<option></option>').html(settings.select)));
+			}
+			$.each(areas,function(i, item){
+				$('#district').append($("<option></option>").attr("value",item.id).html(item.name));
+			});
+			$('#district').bind('change', function(){
+				setAreaValue($('#district').val());
+			});
+		}
+		
+		//设置隐藏域的value,最终传给服务器
+		function setAreaValue(value){
+			if(value){
+				$('#dy_hidden_area_input').attr('value',value);
+			}
+		}
+		
+	};
 	
 	
 })(jQuery);
+
 
 
